33ee95e5dbc9d41bba8fe9f7634ec6ab19b02734
Flávio Mota Gomes <57626755+fmgflavio@users.noreply.github.com>
2021-07-28 14:30:44 -0300

    Update com venda e imovel

--- BackEnd/Controllers/controlleAuth.js
+++ BackEnd/Controllers/controlleAuth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const Imoveis = require('../models/imovel')
+
 const Corretor = require('../models/corretor');
+
+const Venda = require('../models/venda')
 
 const router = express.Router();
 
@@ -8,9 +12,31 @@ router.post('/cadastro_Corretor' , async(req , res) =>{
     try{
         const corretor = await Corretor.create(req.body);
         return res.send({ corretor});
-    }catch(err){
+    }
+    catch(err){
         return res.status(400).send({error: 'Falha ao Registrar'})
     }
 })
+
+router.post('/imoveis' , async(req , res) =>{
+    try{
+        const Imoveis = await Imoveis.create(req.body);
+        return res.send({imoveis});
+    }
+    catch(err){
+        return res.status(400).send({error: 'Falha ao carregar os imoveis'})
+    }
+})
+
+router.post('/venda', async(req, res) =>{
+    try{
+        const Venda = await Venda.create(req.body);
+        return res.send({Venda});
+    }
+    catch(err){
+        return res.status(400).send({error: 'Falha ao carregar as vendas'})
+    }
+})
+
 
 module.exports = app => app.use('/auth' , router);
